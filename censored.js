223ade45bfb99c75bb6a55b2a200763e04059767
Renegade Gandhi <osinayaoludare@gmail.com>
2020-05-15 11:33:26 +0100

    Decipherer

--- censored.js
+++ censored.js
@@ -1,12 +1,28 @@
 
-function notCensored(censored, vowels) {
-    let newCensored = censored.split("*"); //removes all * in the censored word
-    let i = 0;
-    while (i < vowels.length) {
-        result =  vowels[i];
-        console.log(newCensored.join(result)); //joins elements of vowels
-        i++;  
+const notCensored = (censored, vowels) => {
+    //sets initial to 0;
+    let censoredIndex = 0;
+    let vowelsIndex = 0;
+
+    //turns censored word into array;
+    let censoredArr = censored.split("");
+
+    /* Loop through censored word.
+    Replace position of asterisk in censored array,
+     with current index of vowel so no same letter is used twice.
+     */
+    while (censoredIndex <= censored.length){
+        if(censored[censoredIndex] === "*"){
+            censoredArr.splice(censoredIndex, 1, vowels[vowelsIndex]);
+            vowelsIndex++;
+        }
+
+        //move to next index in censored word
+        censoredIndex++;
     }
+
+    //Join censored array after loop is done.
+    return censoredArr.join("");
     
 }
 
